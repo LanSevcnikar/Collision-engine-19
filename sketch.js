163b113b5eb5648e26737ed4989b9aadae3f6503
LanS <lan.sevcnikar@druga.si>
2019-04-01 19:53:05 +0200

    clean up

--- sketch.js
+++ sketch.js
@@ -20,24 +20,24 @@ function draw()	{
 		if(elements[i].type == "ball"){
 			elements[i].show();
 			elements[i].update();
-		}else{
-			elements[i].show();
+			}else{
+				elements[i].show();
+			}
 		}
 	}
-}
 
 
-function round2(n,p){
-	return round(n*pow(10,p))/pow(10,p);
-}
+	function round2(n,p){
+		return round(n*pow(10,p))/pow(10,p);
+	}
 
-function distance_point_line(x0,y0,x1,y1,x2,y2){
-	let pt1 = (y2 - y1) * x0;
-	let pt2 = (x2 - x1) * y0;
-	let pt3 = x2 * y1;
-	let pt4 = y2 * x1;
-	let pt5 = (y2 - y1) * (y2 - y1);
-	let pt6 = (x2 - x1) * (x2 - x1);
-	let dist = (abs(pt1-pt2+pt3-pt4)/sqrt(pt5+pt6));
-	return dist;
-}
+	function distance_point_line(x0,y0,x1,y1,x2,y2){
+		let pt1 = (y2 - y1) * x0;
+		let pt2 = (x2 - x1) * y0;
+		let pt3 = x2 * y1;
+		let pt4 = y2 * x1;
+		let pt5 = (y2 - y1) * (y2 - y1);
+		let pt6 = (x2 - x1) * (x2 - x1);
+		let dist = (abs(pt1-pt2+pt3-pt4)/sqrt(pt5+pt6));
+		return dist;
+	}
