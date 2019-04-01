5b65b9ded75563ba5a821b65c8a65766fccfe7f8
Lan-Sevcnikar <48333598+Lan-Sevcnikar@users.noreply.github.com>
2019-04-01 19:57:21 +0200

    Revert "Ball vall collision testing"

--- mouse.js
+++ mouse.js
@@ -11,17 +11,16 @@ function mouse() {
 		mouse_x_1 = mouseX;
 		mouse_y_1 = mouseY;
 		mouse_pressed_before = true;
-	}
-	else if (mouseIsPressed && mouse_pressed_before){
+	}else if (mouseIsPressed && mouse_pressed_before){
 		mouse_x_2 = mouseX;
 		mouse_y_2 = mouseY;
 		line(mouse_x_1,mouse_y_1,mouse_x_2,mouse_y_2);
-		}else if (!mouseIsPressed && mouse_pressed_before){
-			if(mouse_type === CENTER){
-				elements.push(new Line(mouse_x_1,mouse_y_1,mouse_x_2,mouse_y_2));
-				}else if(mouse_type === LEFT){
-					elements.push(new Ball(mouse_x_1,mouse_y_1,(mouse_x_1-mouse_x_2)*0.05,(mouse_y_1-mouse_y_2)*0.05));
-				}
-				mouse_pressed_before = false;
-			}
+	}else if (!mouseIsPressed && mouse_pressed_before){
+		if(mouse_type === LEFT){
+			elements.push(new Line(mouse_x_1,mouse_y_1,mouse_x_2,mouse_y_2));
+		}else if(mouse_type === CENTER){
+			elements.push(new Ball(mouse_x_1,mouse_y_1,(mouse_x_1-mouse_x_2)*0.05,(mouse_y_1-mouse_y_2)*0.05));
 		}
+		mouse_pressed_before = false;
+	}
+}
