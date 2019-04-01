bfdf5757df711794e496e32f032d3e3f9b71a332
LanS <lan.sevcnikar@druga.si>
2019-04-01 20:22:23 +0200

    Merged by hand, seems to be working but am afraid that there are bugs still present

--- sketch.js
+++ sketch.js
@@ -5,7 +5,9 @@ var g = 0.3;
 var elements = [];
 var hasnumber = 0;
 var bounce = 0.75;
-var friction = 0.95;
+var friction = 0.99;
+var air_friction = 0.998;
+var ballballbounce = 0.2;
 
 function setup() {
 	mouse_pressed_before = false;
@@ -20,24 +22,37 @@ function draw()	{
 		if(elements[i].type == "ball"){
 			elements[i].show();
 			elements[i].update();
-			}else{
-				elements[i].show();
-			}
+		}
+		else{
+			elements[i].show();
 		}
 	}
+}
 
 
-	function round2(n,p){
-		return round(n*pow(10,p))/pow(10,p);
-	}
+function round2(n,p){
+	return round(n*pow(10,p))/pow(10,p);
+}
 
-	function distance_point_line(x0,y0,x1,y1,x2,y2){
-		let pt1 = (y2 - y1) * x0;
-		let pt2 = (x2 - x1) * y0;
-		let pt3 = x2 * y1;
-		let pt4 = y2 * x1;
-		let pt5 = (y2 - y1) * (y2 - y1);
-		let pt6 = (x2 - x1) * (x2 - x1);
-		let dist = (abs(pt1-pt2+pt3-pt4)/sqrt(pt5+pt6));
-		return dist;
-	}
+function distance_point_line(x0,y0,x1,y1,x2,y2){
+	let pt1 = (y2 - y1) * x0;
+	let pt2 = (x2 - x1) * y0;
+	let pt3 = x2 * y1;
+	let pt4 = y2 * x1;
+	let pt5 = (y2 - y1) * (y2 - y1);
+	let pt6 = (x2 - x1) * (x2 - x1);
+	let dist = (abs(pt1-pt2+pt3-pt4)/sqrt(pt5+pt6));
+	return dist;
+}
+
+function distance_point_point(x1,y1,x2,y2){
+	return sqrt((x1-x2)*(x1-x2)+(y1-y2)*(y1-y2));
+}
+
+function distance_vec_vec(v1,v2){
+	let x1 = v1.x;
+	let x2 = v2.x;
+	let y1 = v1.y;
+	let y2 = v2.y;
+	return sqrt((x1-x2)*(x1-x2)+(y1-y2)*(y1-y2));
+}
